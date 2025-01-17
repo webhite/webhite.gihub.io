a6151b283299a10b0235879fd3444c3b8aca936c
Parvej Khan <parvejkhan@Parvejs-MacBook-Air.local>
2025-01-18 00:34:12 +0530

    removed the profile photo

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -63,11 +63,12 @@ const Nav = () => {
                 <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">Open user menu</span>
-                  <img
+                  {/* <img
                     alt=""
                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                     className="size-8 rounded-full"
-                  />
+                  /> */}
+                  <h1 className='text-xl font-bold text-white'>Webhite</h1>
                 </MenuButton>
               </div>
               <MenuItems
